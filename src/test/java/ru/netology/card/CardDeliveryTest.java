632163fa4bb23258dd0c795069b82deed05e1b4d
Дмитрий Куулар <kuulardmo@bk.ru>
2022-10-23 21:51:53 +0700

    Fixed the second test

--- src/test/java/ru/netology/card/CardDeliveryTest.java
+++ src/test/java/ru/netology/card/CardDeliveryTest.java
@@ -13,8 +13,19 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
-    String generateDate(int days){
+    String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String actualDate() {
+        return LocalDate.now().format(DateTimeFormatter.ofPattern("MM"));
+    }
+
+    String considerDateMonth(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("MM"));
+    }
+    String considerDateDay(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("d"));
     }
 
     @Test
@@ -22,23 +33,7 @@ public class CardDeliveryTest {
         String date = generateDate(4);
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(date);
-        $("[data-test-id=name] input").setValue("Петр Василий");
-        $("[data-test-id=phone] input").setValue("+78883331313");
-        $("[data-test-id=agreement]").click();
-        $(".button__text").click();
-        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на "
-        + date), Duration.ofSeconds(15)).shouldBe(visible);
-    }
-
-    @Test
-    void shouldFillFormUsingHelpButtons() throws InterruptedException {
-        String date = generateDate(3);
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Са");
-        $$(".menu-item__control").find(exactText("Санкт-Петербург")).click();
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(date);
         $("[data-test-id=name] input").setValue("Петр Василий");
         $("[data-test-id=phone] input").setValue("+78883331313");
@@ -46,6 +41,29 @@ public class CardDeliveryTest {
         $(".button__text").click();
         $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на "
                 + date), Duration.ofSeconds(15)).shouldBe(visible);
-        Thread.sleep(8000);
+    }
+
+    @Test
+    void shouldFillFormUsingHelpButtons() {
+        int testDays = 7;
+        String date = generateDate(testDays);
+        String dateMonth = considerDateMonth(testDays);
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Са");
+        $$(".menu-item__control").find(exactText("Санкт-Петербург")).click();
+        $(".input__icon").click();
+        boolean result = actualDate().equals(dateMonth);
+        if (result == true) {
+            $$(".calendar__day").find(exactText(considerDateDay(testDays))).click();
+        } else {
+            $(".calendar__arrow[data-step='1']").click();
+            $$(".calendar__day").find(exactText(considerDateDay(testDays))).click();
+        }
+        $("[data-test-id=name] input").setValue("Петр Василий");
+        $("[data-test-id=phone] input").setValue("+78883331313");
+        $("[data-test-id=agreement]").click();
+        $(".button__text").click();
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на "
+                + date), Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
